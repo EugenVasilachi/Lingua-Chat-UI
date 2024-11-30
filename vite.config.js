890442fe4a0e2b11dfdf405bd2d9fff4a1e79538
Vasilachi Eugen <vasilachi38@yahoo.com>
2024-11-30 19:06:19 +0200

    sending the environment variables as arguments in Dockerfile

--- vite.config.js
+++ vite.config.js
@@ -1,26 +1,8 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import { loadEnv } from "vite";
 
 export default defineConfig(({ mode }) => {
-  // Load environment variables based on the current mode (e.g., development or production)
-  const env = loadEnv(mode, process.cwd(), "");
-
   return {
     plugins: [react()],
-    define: {
-      // Expose environment variables to the app
-      "process.env": {
-        VITE_API_KEY: env.VITE_API_KEY,
-        VITE_AUTH_DOMAIN: env.VITE_AUTH_DOMAIN,
-        VITE_DATABASE_URL: env.VITE_DATABASE_URL,
-        VITE_PROJECT_ID: env.VITE_PROJECT_ID,
-        VITE_STORAGE_BUCKET: env.VITE_STORAGE_BUCKET,
-        VITE_MESSAGING_SENDER_ID: env.VITE_MESSAGING_SENDER_ID,
-        VITE_APP_ID: env.VITE_APP_ID,
-        VITE_MEASUREMENT_ID: env.VITE_MEASUREMENT_ID,
-        VITE_FLASK_BACKEND_URL: env.VITE_FLASK_BACKEND_URL,
-      },
-    },
   };
 });
