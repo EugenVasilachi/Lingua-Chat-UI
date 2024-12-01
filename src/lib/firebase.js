c486f6c2783d09afae20d85aceb61569b7036720
Vasilachi Eugen <vasilachi38@yahoo.com>
2024-12-01 20:42:02 +0200

    console log firebase config

--- src/lib/firebase.js
+++ src/lib/firebase.js
@@ -14,6 +14,8 @@ const firebaseConfig = {
   measurementId: import.meta.env.VITE_MEASUREMENT_ID,
 };
 
+console.log("firebaseConfig", firebaseConfig);
+
 const app = initializeApp(firebaseConfig);
 
 export const auth = getAuth(app);
